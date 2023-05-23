08e0c85cc7576bdac709bd201ce27f9e3a909d44
abdullah <abdullah.myt412@gmail.com>
2023-05-23 09:16:33 +0300

    add sidbar and navbar folders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Good Morning X3 Team</h1>
+      <h2>I'am Changing</h2>
     </div>
   );
 }
